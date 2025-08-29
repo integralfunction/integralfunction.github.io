39a87957b879e2e10ea2a62de3fdff78baf7e165
integralfunction <83551660+integralfunction@users.noreply.github.com>
2025-08-28 21:15:20 -0400

    ?

---
+++ app/app.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <UApp>
+      <header class="py-2 px-2 justify-between flex flex-row">
+        <div></div>
+        <div></div>
+        <a class="p-2 bg-black inline-flex rounded-full align-middle" href="https://github.com/integralfunction">
+          <UIcon name="i-lucide-github" class="size-6 bg-white" />
+        </a>
+      </header>
+      <NuxtPage />
+      <footer class="h-8">
+      </footer>
+    </UApp>
+  </div>
+</template>
